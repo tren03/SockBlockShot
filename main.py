37c0c18ad05db048399da622317388088c8a6541
bmsce <vishnusethuraman@gmail.com>
2024-05-15 17:09:47 +0530

    added restart and quit in gameloop after game ends

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from player1 import Player1
 from projectile import Projectile
 from player2 import Player2
 
+pygame.font.init()
+
 width = 1000
 height = 1000
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
+font = pygame.font.Font(None, 36)
 
 clientNumber = 0
 
@@ -23,6 +26,20 @@ def redrawWindow(win,player1,player2,bullets1,bullets2):
     pygame.display.update()
 
 
+def display_popup(message):
+    popup_text = font.render(message, True, (255, 255, 255))
+    popup_rect = popup_text.get_rect(center=(width // 2, height // 2))
+    
+    # Draw background for the popup
+    pygame.draw.rect(win, (0, 0, 0), (popup_rect.x - 10, popup_rect.y - 10, popup_rect.width + 20, popup_rect.height + 20))
+    
+    # Draw the popup text
+    win.blit(popup_text, popup_rect)
+    
+    pygame.display.update()
+
+
+
 
 
 def main():
@@ -35,7 +52,9 @@ def main():
     can_shoot1 = True
     can_shoot2 = True
     shoot_delay = 90 # Adjust this value to control the firing rate (in milliseconds)
-    shoot_timer = 0
+    shoot_timer1 = 0
+    shoot_timer2 = 0
+    state = 1
 
     while run:
         clock.tick(60)
@@ -67,8 +86,10 @@ def main():
             p2_rect = pygame.Rect(p2.x,p2.y,p2.width,p2.height)
             if p2_rect.colliderect(bullet_rect):
                 bullets1.remove(bullet)
-                if(p2.health < 0):                    
-                    pygame.quit()
+                if(p2.health < 0):   
+                    state = 0
+                    w = "p1"                
+                    
                 else:
                     p2.health -= 10
 
@@ -81,7 +102,8 @@ def main():
             if p1_rect.colliderect(bullet_rect):
                 bullets2.remove(bullet)
                 if(p1.health < 0):
-                    pygame.quit()
+                    state = 0
+                    w = "p2"
                 else:
                     p1.health -= 10
         
@@ -103,10 +125,29 @@ def main():
         if not can_shoot2 and pygame.time.get_ticks() - shoot_timer2 >= shoot_delay:
             can_shoot2 = True
 
+
+        if(state == 0):
+            display_popup(f"{w} won the game \n Press q to quit or r to restart")
+         
+            if keys[pygame.K_q]:
+                
+                pygame.quit()
+            if keys[pygame.K_r]:
+                p1 = Player1(10, 10, 50, 50, 'red')
+                p2 = Player2(0,height-100,50,50,'blue')
+                bullets1.clear()
+                bullets2.clear()
+                state = 1
+                
+        else:
+            p1.move()
+            p2.move()
+            redrawWindow(win, p1,p2, bullets1,bullets2)
+
        
-        p1.move()
-        p2.move()
-        redrawWindow(win, p1,p2, bullets1,bullets2)
+
+
+        
 
 main()
 
